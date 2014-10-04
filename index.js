982c1b51a6c13dfe870ed1623cdaf12fc8d9994d
Kjell <kjell@leanside.com>
2014-10-03 21:43:59 -0500

    Upload big images to twitter
    this is a PITA because of oauth 1, which requires all requests to
    through a proxy 'auth-server' to keep the oauth client secret. So when
    uploading an image, every byte of data is relayed to twitter via the
    auth server. So it takes 2x as long.
    
    (Facebook takes the image as a URL and fetches it herself)

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ window.images = {
 }
 
 window.art = JSON.parse(fs.readFileSync('art.json', 'utf8'))
+
+window.loadImage = function(id, callback) {
+  var image = new Image()
+  image.src = 'images/'+id+'-1000.jpg'
+  image.onload = function() {
+    var canvas = document.createElement('canvas'), ctx = canvas.getContext('2d')
+    canvas.width = image.width
+    canvas.height = image.height
+    ctx.drawImage(image,0,0)
+    callback(null, canvas.toDataURL())
+  }
+}
